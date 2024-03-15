fb4acf023e518480617ea4d967f4ae4321b591ce
AMALJITH THOMAS <amaljithkthomas@gmail.com>
2024-03-15 22:57:46 +0530

    animated the fav icon

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -33,7 +33,19 @@ class MealDetailScreen extends ConsumerWidget {
                   ),
                 );
               },
-              icon: Icon(isFav ? Icons.star : Icons.star_border),
+              icon: AnimatedSwitcher(
+                duration: const Duration(milliseconds: 300),
+                transitionBuilder: (child, animation) {
+                  return RotationTransition(
+                    turns: animation,
+                    child: child,
+                  );
+                },
+                child: Icon(
+                  isFav ? Icons.star : Icons.star_border,
+                  key: ValueKey(isFav),
+                ),
+              ),
             )
           ],
         ),
